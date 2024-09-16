41ffa755d51c8d0a9ccd91961efe96d22f2a2a57
VadimKizev <kizev.vadim@yandex.ru>
2024-09-16 20:56:52 +0300

    1 урок

---
+++ базовые структуры данных.py
@@ -0,0 +1,19 @@
+#1st programm
+print(9 ** 0.5 * 5)
+
+#2nd programm
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+#3rd programm
+a = 2
+b = a * a + a
+c = a * (a + a)
+print(b, c, b == c)
+
+#4th programm
+a = "123.456"
+b = float(a)
+c = b * 10
+d = int(c)
+e = d % 10
+print(e)
